079722d6bc2bd32f83dfc3abf9ec0bf5c1b062ef
Damon Brown <dbrow437@gmail.com>
2018-02-06 17:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function add2(n) {
 var funkyFunction = function() {
   return function() {
      var theFunk = funkyFunction()
-     var funckyFunction = "FUNKY!"
+     var funkyFunction = "FUNKY!"
     return "FUNKY!"
   }
 }
